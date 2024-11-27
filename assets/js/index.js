a9a658ddaeb56c3722327b2b3ee2a84bffd00ce5
Шичков Александр <ctorkey@gmail.com>
2024-11-27 18:07:58 +0300

    best-games and header are done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
-"use strict"
+'use strict'
+
+// BG
+
+const mainBg = document.querySelector('.main__bg')
+const main = document.querySelector('.main')
+
+const mainHeight = main.offsetHeight
+console.log(mainHeight)
+
+for (let i = 0; i < mainHeight; i += 500) {
+	let bgDuo = document.createElement('div')
+	bgDuo.className = 'main__bg-duo'
+	let bgImg1 = document.createElement('img')
+	let bgImg2 = document.createElement('img')
+	bgImg1.className = 'main__bg-img'
+	bgImg2.className = 'main__bg-img'
+	bgImg1.src = './assets/images/bg.png'
+	bgImg2.src = './assets/images/bg.png'
+	bgImg1.style.transform = `rotate(${Math.random() * 360}deg)`
+	bgImg2.style.transform = `rotate(${Math.random() * 360}deg)`
+	bgDuo.appendChild(bgImg1)
+	bgDuo.appendChild(bgImg2)
+	mainBg.appendChild(bgDuo)
+}
